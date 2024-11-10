0626ea250d8365a00b8b1ba95d5a09f86bf56e04
Shrutipriya07 <priyaa.shruti147@gmail.com>
2024-11-10 19:52:02 +0100

    Update code

--- cypress/support/search.cmd.js
+++ cypress/support/search.cmd.js
@@ -1,11 +1,13 @@
 import { SearchPage } from '../selectors/searchPage';
 Cypress.Commands.add('openSearch', () => {
-    cy.get(SearchPage.elements.search.icon).first().click({ force: true });
+    cy.get(SearchPage.elements.search.icon).first().click({
+        force: true
+    });
     cy.get(SearchPage.elements.search.wrapper).should('be.visible');
     cy.get(SearchPage.elements.search.screenReader).should('have.text', 'Search');
-  });
-  
-  Cypress.Commands.add('performSearch', (searchTerm) => {
+});
+
+Cypress.Commands.add('performSearch', (searchTerm) => {
     cy.openSearch();
     cy.get('.search-field').type(`${searchTerm}{enter}`);
-  });
+});
